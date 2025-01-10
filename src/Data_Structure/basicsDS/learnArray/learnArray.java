af5fbff361b22917cf0f55cc008ec9ff9e65c181
Ashish jha <ajha5645@gmail.com>
2025-01-10 22:27:45 +0530

    array.md file

--- src/Data_Structure/basicsDS/learnArray/learnArray.java
+++ src/Data_Structure/basicsDS/learnArray/learnArray.java
@@ -3,15 +3,38 @@ package Data_Structure.basicsDS.learnArray;
 public class learnArray {
 
     public static void main(String[] args) {
-        int[] arr = {21, 42, 0, 21, 1, 3, 12, 6, 2};
-        int maxNumber = arr[0];
+        int[] arr = {12, 35, 1, 10, 34, 1};
+        int maxNumber = findMaxNumber(arr);
+        System.out.println("Max number in the array is: " + maxNumber);
 
+        int secondLargest = findSecondLargestNumber(arr);
+        System.out.println("Second largest number in the array is: " + secondLargest);
+    }
+
+    //    find max number optimal solutions
+    public static int findMaxNumber(int[] arr) {
+        int maxNumber = arr[0];
         for (int element : arr) {
             if (element > maxNumber) {
                 maxNumber = element;
             }
         }
-        System.out.println("Max number in the array is: " + maxNumber);
+        return maxNumber;
+    }
 
+    //    find second largest or smallest number in the array without sorting
+    public static int findSecondLargestNumber(int[] arr) {
+        int largest = arr[0];
+        int secondLargest = -1;
+
+        for (int element : arr) {
+            if (element > largest) {
+                secondLargest = largest;
+                largest = element;
+            } else if (element < largest && element > secondLargest) {
+                secondLargest = element;
+            }
+        }
+        return secondLargest;
     }
 }
