9caa9c54e4e05404df1cfb870f8cc5e0216adc10
Ashish jha <ajha5645@gmail.com>
2025-01-10 21:11:14 +0530

    array.md file

--- src/Data_Structure/basicsDS/learnArray/learnArray.java
+++ src/Data_Structure/basicsDS/learnArray/learnArray.java
@@ -1,13 +1,17 @@
 package Data_Structure.basicsDS.learnArray;
 
-import java.util.Arrays;
-
 public class learnArray {
 
     public static void main(String[] args) {
-        int
         int[] arr = {21, 42, 0, 21, 1, 3, 12, 6, 2};
-        int max = Arrays.stream(arr).max().getAsInt();
-        System.out.println(max);
+        int maxNumber = arr[0];
+
+        for (int element : arr) {
+            if (element > maxNumber) {
+                maxNumber = element;
+            }
+        }
+        System.out.println("Max number in the array is: " + maxNumber);
+
     }
 }
