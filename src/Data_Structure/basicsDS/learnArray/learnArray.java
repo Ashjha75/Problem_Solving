96fb8ee37eae181b560d671c406bfcd497dc5f1e
Ashish jha <ajha5645@gmail.com>
2025-01-10 20:33:17 +0530

    some basic commit

--- src/Data_Structure/basicsDS/learnArray/learnArray.java
+++ src/Data_Structure/basicsDS/learnArray/learnArray.java
@@ -1,27 +1,13 @@
 package Data_Structure.basicsDS.learnArray;
 
+import java.util.Arrays;
+
 public class learnArray {
 
     public static void main(String[] args) {
-        // Array declaration
-        int[] arr = new int[5];
-        // Array initialization
-        arr[0] = 1;
-        arr[1] = 2;
-        arr[2] = 3;
-        arr[3] = 4;
-        arr[4] = 5;
-
-        // Accessing elements of an array
-        System.out.println(arr[0]);
-        System.out.println(arr[1]);
-        System.out.println(arr[2]);
-        System.out.println(arr[3]);
-        System.out.println(arr[4]);
-
-        // Array traversal
-        for (int i = 0; i < arr.length; i++) {
-            System.out.println(arr[i]);
-        }
+        int
+        int[] arr = {21, 42, 0, 21, 1, 3, 12, 6, 2};
+        int max = Arrays.stream(arr).max().getAsInt();
+        System.out.println(max);
     }
 }
